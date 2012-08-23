637dd10a99d6d2ef808094f162db679cfd895ae0
Vladimir <vladimir.programmer@gmail.com>
2012-08-23 20:04:44 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theideaslab::Application.config.secret_token = '19dc976377d10e009dfd19cfe422e1ce9a728c7a84480f020e390b5e16024572c0cbcadce6075cbcf368e356c9178decb00f84800f3b1a758343e6d4f1d8f91f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Theideaslab::Application.config.session_store :cookie_store, key: '_theideaslab_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Theideaslab::Application.config.session_store :active_record_store
